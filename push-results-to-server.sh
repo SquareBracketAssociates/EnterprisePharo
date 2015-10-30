d4c5031ee42e13a6b42b042e633da57114390013
Damien Cassou <damien@cassou.me>
2015-10-30 10:39:23 +0100

    simplify push-results-to-server

--- push-results-to-server.sh
+++ push-results-to-server.sh
@@ -17,31 +17,9 @@ rm -rf $TMP_DIR
 mkdir $TMP_DIR
 EOF
 
-rm -rf to-push
-mkdir -p to-push/html/figures
-cp book-result/EnterprisePharo.pdf to-push/
-
-# Push all css/js files
-cp -R support/html/* to-push/html
-
-# Push all .html chapters
-for chapter in $chapters; do
-    html="${chapter%%.*}.html"
-
-    # Hack to change the references to support files (e.g., .css, .js)
-    sed 'sX../support/html/XX' $html > to-push/html/$(basename $html)
-
-    # copy the figures
-    cp -R $(dirname $html)/figures/* to-push/html/figures/ 2>/dev/null || true
-done
-
-cd to-push/html
-
-cd ../..
-
 echo Pushing all files to $SERVER
-scp -r to-push $SERVER:$TMP_DIR
+scp -r book-result $SERVER:$TMP_DIR
 
 ssh $SERVER sh <<EOF
-sudo su --login --command "cp -R $TMP_DIR/to-push/* $BASEDIR" filepharosync
+sudo su --login --command "cp -R $TMP_DIR/book-result/* $BASEDIR" filepharosync
 EOF
