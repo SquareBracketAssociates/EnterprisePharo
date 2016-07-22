749b6cd6b8cbe8fb022149a4c749b40ee185f68f
ThibaultAr <thibault.arloing@hotmail.fr>
2016-07-22 16:01:44 +0200

    update subdownload.sh to use stable version

--- subdownload.sh
+++ subdownload.sh
@@ -1,1 +1,1 @@
-wget -qO- "https://raw.githubusercontent.com/pillar-markup/pillar/master/download.sh" | bash -s dev
+wget -qO- "https://raw.githubusercontent.com/pillar-markup/pillar/master/download.sh" | bash -s $*
