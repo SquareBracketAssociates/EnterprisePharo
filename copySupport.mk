ad75047efb3febac0ce17a5ee3c27ebfb5e153d2
Damien Pollet <damien.pollet@gmail.com>
2016-10-03 17:49:15 +0200

    Quick fix support copy

--- copySupport.mk
+++ copySupport.mk
@@ -1,4 +1,4 @@
 copySupport: initDir
 	find . -type d -path $(OUTPUTDIRECTORY) -prune -o -wholename "*/figures" -exec rsync -rR {} $(OUTPUTDIRECTORY) \;
-	cp -r support/ $(OUTPUTDIRECTORY)
+	cp -R support $(OUTPUTDIRECTORY)
 	cp .latexmkrc $(OUTPUTDIRECTORY)
